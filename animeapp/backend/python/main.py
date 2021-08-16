fab6d70cbf32b3d6e497e6d40901b900c8e0893e
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-08-17 01:55:50 +0900

    Fix Docker , Fix main.py

--- animeapp/backend/python/main.py
+++ animeapp/backend/python/main.py
@@ -14,6 +14,7 @@ def hello(year,cool):
     headers = {"content-type": "application/json"}
     response=requests.get('http://api.moemoe.tokyo/anime/v1/master/'+year+'/'+cool, headers=headers)
     json_data = response.json()
+    json_data_addOGPimage = []
     #print(json_data)
     for data in json_data:
         print(data['public_url'])
@@ -21,16 +22,26 @@ def hello(year,cool):
             url = data['public_url']
             html_page = urlopen(url)
             soup = BeautifulSoup(html_page, 'html.parser')
+            og_description = soup.find('meta', attrs={'property': 'og:description', 'content': True})
             og_img = soup.find('meta', attrs={'property': 'og:image', 'content': True})
             if og_img is not None:
+                data['ogp_image_url'] = og_img['content']
+                data['ogp_description'] = og_description['content']
+                print(og_description['content'])
                 print(og_img['content'])
             else:
                 print('!!not found og:image tag!!')
+                data['ogp_image_url'] = 'not_found'
+                data['ogp_description'] = 'not_found'
         except:
             print('!!not found og:image tag!!')
+            data['ogp_image_url'] = 'not_found'
+            data['ogp_description'] = 'not_found'
             pass
+        print(data)
+        json_data_addOGPimage.append(data)
         
-    return jsonify(json_data)
+    return jsonify(json_data_addOGPimage)
 
 if __name__ == "__main__":
     app.run()
